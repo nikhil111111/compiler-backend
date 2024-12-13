11023e2509c3d6f41937c266742b59dade84a3e8
Nikhil Garg <nikhil.garg.2509@gmail.com>
2024-12-13 16:12:36 +0530

    cors me url daala h

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "https://frontendcompiler.vercel.app" }));
 config();
 
 app.use("/compiler", compilerRouter);
